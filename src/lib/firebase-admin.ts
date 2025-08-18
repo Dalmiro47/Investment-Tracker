e394d9a6d6d30e1b120ee4b10f08641f23116201
Dalmiro Dos Santos <dalmefr@gmail.com>
2025-08-18 17:59:09 +0000

    when I try to run a simulation it failed, what could the issue be?

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,10 +1,8 @@
 // src/lib/firebase-admin.ts
-import { getApps, initializeApp, applicationDefault } from 'firebase-admin/app';
+import { getApps, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
 if (!getApps().length) {
-  initializeApp({
-    credential: applicationDefault(), // or cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON!))
-  });
+  initializeApp();
 }
 export const adminDb = getFirestore();
